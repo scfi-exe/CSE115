5c0969d653063724638059a37dcd140ec929ed2a
scfi-exe <scottfinkelstein97@gmail.com>
2025-09-05 14:36:51 -0400

    recitation 1

---
+++ CSE115/Recitations/Class 1/memoryDiagram.py
@@ -0,0 +1,19 @@
+fahrenheit = 95
+feels = "hot"
+celsius = 35
+kelvin = celsius
+degrees = 3.1415
+print("feels")
+print(feels)
+kelvin = 308.15
+
+
+def ageCount():
+    total = 0
+    for age in range(4):
+        age = int(input("What is your age?: "))
+        total = total + age
+    print(f"The combined total of the ages is: {total}")
+
+
+ageCount()
