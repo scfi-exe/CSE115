ff56c4a7f2224396519aec5f76f2dff1ddc72fd0
scfi-exe <scottfinkelstein97@gmail.com>
2025-09-12 14:21:08 -0400

    Recitation 2

---
+++ CSE115/Recitations/Class 1/class2memDiagram.py
@@ -0,0 +1,13 @@
+import math
+
+height = 88
+feet = height // 12
+print(f"feet: {feet}")
+inches = height % 12
+print(inches)
+height = 100
+cm = height * 2.54
+met = math.floor(height / 100)
+exp = (2**4) + (12 - 1)
+print(100 * 2.54)
+print(f"exp = {(2**4) + (12-1)}")
